0d21a1fe103329bd742134bb8aa8a0f0af81edbe
HP <HP@DESKTOP-M002ELQ>
2022-01-30 15:20:39 -0500

    Added: creado el metodo sumar

---
+++ src/main/java/ec/demos/Operaciones.java
@@ -0,0 +1,55 @@
+package ec.demos;
+
+public class Operaciones {
+	private int value1;
+	private int value2;
+	
+	
+	
+	public Operaciones() {
+		super();
+		this.value1=0;
+		this.value2=0;
+	}
+
+
+
+	public Operaciones(int value1, int value2) {
+		super();
+		this.value1 = value1;
+		this.value2 = value2;
+	}
+
+
+
+	public void sumar() {
+		int suma=value1+value2;
+		System.out.print("Suma : "+suma);
+	}
+
+
+
+	public int getValue1() {
+		return value1;
+	}
+
+
+
+	public void setValue1(int value1) {
+		this.value1 = value1;
+	}
+
+
+
+	public int getValue2() {
+		return value2;
+	}
+
+
+
+	public void setValue2(int value2) {
+		this.value2 = value2;
+	}
+	
+	
+}
--- src/main/java/ec/demos/OperacionesApp.java
+++ src/main/java/ec/demos/OperacionesApp.java
@@ -3,7 +3,10 @@ package ec.demos;
 public class OperacionesApp {
 
 	public static void main(String[] args) {
-		System.out.print("Hola");
+		Operaciones operaciones=new Operaciones(10, 20);
+		operaciones.setValue1(50);
+		operaciones.setValue2(63);
+		operaciones.sumar();
 	}
 
 }
